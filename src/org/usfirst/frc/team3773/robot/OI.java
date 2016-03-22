adba9b4bee53f346b58eee26884c77f3e4c4ab18
FRC3773 <3773programming@gmail.com>
2016-03-22 08:32:33 -0400

    revmoved the axis

--- src/org/usfirst/frc/team3773/robot/OI.java
+++ src/org/usfirst/frc/team3773/robot/OI.java
@@ -17,12 +17,12 @@ public class OI { //Inputs as private or not?
     Joystick rightJoystick = new Joystick(RobotMap.rightJoystickChannel);
     Joystick xboxController = new Joystick(RobotMap.xboxChannel);
     //Not sure if it is correct to put diff axis here and then use an accessor to pull them for commands
-    double driveStickY = rightJoystick.getY();
-    double driveStickX = rightJoystick.getX();
-    boolean driveTrigger = rightJoystick.getRawButton(1);
+    //double driveStickY = rightJoystick.getY();
+    //double driveStickX = rightJoystick.getX();
+    //boolean driveTrigger = rightJoystick.getRawButton(1);
     JoystickButton driveStick2 = new JoystickButton(rightJoystick, 2); //Button 2 on the driveJoystick
     JoystickButton driveStick3 = new JoystickButton(rightJoystick, 3);
-    double sens = (-rightJoystick.getZ() + 1) / 2.0; //Sets the sensitivity of the drive joystick
+    //double sens = (-rightJoystick.getZ() + 1) / 2.0; //Sets the sensitivity of the drive joystick
     //Xbox controller
     JoystickButton xboxA = new JoystickButton(xboxController, 1); //A button
     JoystickButton xboxB = new JoystickButton(xboxController, 2); //B button
@@ -30,13 +30,13 @@ public class OI { //Inputs as private or not?
     JoystickButton xboxY = new JoystickButton(xboxController, 4); //Y button
     JoystickButton xboxRB = new JoystickButton(xboxController, 6); //Right Bumper
     JoystickButton xboxLB = new JoystickButton(xboxController, 5); //Left Bumper
-    double xboxLX = xboxController.getRawAxis(0); //Left joystick x-axis?
+    //double xboxLX = xboxController.getRawAxis(0); //Left joystick x-axis?
     //double xboxLY = xboxController.getRawAxis(1); //Left joystick y-axis
-    double xboxRX = xboxController.getRawAxis(4); //Right joystick x-axis
-    double xboxRY = xboxController.getRawAxis(5); //Right joystick y-axis
+    //double xboxRX = xboxController.getRawAxis(4); //Right joystick x-axis
+    //double xboxRY = xboxController.getRawAxis(5); //Right joystick y-axis
     JoystickButton xboxStart = new JoystickButton(xboxController, 9); //Start button
-    double xboxLT = xboxController.getRawAxis(2); //Left trigger //check axis number
-    double xboxRT = xboxController.getRawAxis(3); //Right trigger //check axis number
+    //double xboxLT = xboxController.getRawAxis(2); //Left trigger //check axis number
+    //double xboxRT = xboxController.getRawAxis(3); //Right trigger //check axis number
         
     public OI() {
     	// Put Some buttons on the SmartDashboard
@@ -60,19 +60,17 @@ public class OI { //Inputs as private or not?
     public Joystick getRightJoystick() {
     	return rightJoystick;
     }
-    //Check
-    public boolean getDriveTrigger(){
-    	return driveTrigger;
-    }
-    
+
     public Joystick getXboxController() {
     	return xboxController;
     }
     //Check
+    /**
     public double getXboxLeftJoystickYAxis(){
     	if(Math.abs(xboxLY) <= .25)
         	xboxLY = 0.0;
     	return xboxLY;
+    */	
     }
 }
 
