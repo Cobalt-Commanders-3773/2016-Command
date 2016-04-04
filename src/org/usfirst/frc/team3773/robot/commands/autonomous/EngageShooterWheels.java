8de39b9ec65056d1a795d3aa6f6f390792fb4250
Ryan <3773programming@gmail.com>
2016-04-04 13:08:53 -0400

    Before first test

--- src/org/usfirst/frc/team3773/robot/commands/autonomous/EngageShooterWheels.java
+++ src/org/usfirst/frc/team3773/robot/commands/autonomous/EngageShooterWheels.java
@@ -9,11 +9,11 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class EngageShooterWheels extends Command {
 
-    public EngageShooterWheels(double delay) {
+    public EngageShooterWheels() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.shooter);
-    	setTimeout(delay);
+    	//setTimeout(delay);
     }
 
     // Called just before this Command runs the first time
@@ -27,15 +27,17 @@ public class EngageShooterWheels extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return isTimedOut();
+        return false;
     }
 
     // Called once after isFinished returns true
     protected void end() { //Should not stop the shooter wheels so that they will be up to speed later
+        Robot.shooter.stopShooter();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        Robot.shooter.stopShooer();
     }
 }
