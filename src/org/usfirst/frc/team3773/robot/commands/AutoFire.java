f00e379abf25f272c7551ae3e8287ef97b074db6
FRC3773 <3773programming@gmail.com>
2016-04-12 08:23:31 -0400

    removed drive

--- src/org/usfirst/frc/team3773/robot/commands/AutoFire.java
+++ src/org/usfirst/frc/team3773/robot/commands/AutoFire.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoFire extends CommandGroup {
     
     public  AutoFire() {
-    	addSequential(new DriveForward(2, -.85));
+    	//addSequential(new DriveForward(2, -.85));
     	addSequential(new AutoShoot(1));
     	addParallel(new AutoIntake(1));
     	addSequential(new AutoShoot(1));
