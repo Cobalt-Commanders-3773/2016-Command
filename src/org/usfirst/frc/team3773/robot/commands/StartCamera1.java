167472a553abe9f645df499e63fa9fc2fb25b66d
FRC3773 <3773programming@gmail.com>
2016-04-12 08:25:37 -0400

    moved setCamera1 to init

--- src/org/usfirst/frc/team3773/robot/commands/StartCamera1.java
+++ src/org/usfirst/frc/team3773/robot/commands/StartCamera1.java
@@ -17,11 +17,11 @@ public class StartCamera1 extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        Robot.camera.setCamera1();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.camera.setCamera1();
     }
 
     // Make this return true when this Command no longer needs to run execute()
