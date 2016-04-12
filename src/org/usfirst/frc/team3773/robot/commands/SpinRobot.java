b75780c5b6e14bc956d5740e9cf35c46fede7953
FRC3773 <3773programming@gmail.com>
2016-04-12 08:21:09 -0400

    Create SpinRobot.java

---
+++ src/org/usfirst/frc/team3773/robot/commands/SpinRobot.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team3773.robot.commands;
+
+import org.usfirst.frc.team3773.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveForward extends Command {
+	
+	private double speed;
+	private double time;
+
+    public DriveForward(double time, double speed) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.drivetrain);
+    	this.speed = speed;
+    	this.time = time;
+    }
+
+	// Called just before this Command runs the first time
+    protected void initialize() {
+    	setTimeout(this.time);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.drivetrain.tankDrive(this.speed, -this.speed);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.drivetrain.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
