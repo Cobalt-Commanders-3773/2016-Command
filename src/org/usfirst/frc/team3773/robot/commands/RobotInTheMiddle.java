4efc39341e3b734bca27a7763b558446037566ed
FRC3773 <3773programming@gmail.com>
2016-04-12 08:35:13 -0400

    Update RobotInTheMiddle.java

--- src/org/usfirst/frc/team3773/robot/commands/RobotInTheMiddle.java
+++ src/org/usfirst/frc/team3773/robot/commands/RobotInTheMiddle.java
@@ -7,7 +7,14 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class RobotInTheMiddle extends CommandGroup {
     
+    private Random randGen;
+    private int time;
+    
     public  RobotInTheMiddle() {
         //Robot spins in a circle for a random amount of time and then shoots the ball
+        randGen = new Random();
+        this.time = randGen.nextInt(10);
+        addSequential(new SpinRobot(this.time, .5));
+        addSequential(new AutoFire());
     }
 }
